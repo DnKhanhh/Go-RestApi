e80856510f243729a2b028adbd84fd1ecc9e406a
Brian <dnkhanhdev@gmail.com>
2024-03-27 10:00:04 +0700

    format code

--- restapi/main.go
+++ restapi/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"net/http"
-
 	"example.com/restapi/db"
-	"example.com/restapi/models"
+	"example.com/restapi/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,37 +10,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8082")
-}
-
-func getEvents(ctx *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
-		return
-	}
-	ctx.JSON(http.StatusOK, events)
-}
-
-func createEvent(ctx *gin.Context) {
-	var event models.Event
-	err := ctx.ShouldBindJSON(&event)
-
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
-		return
-	}
-
-	event.UserID = 1
-
-	err = event.Save()
-
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create events. Try again later."})
-		return
-	}
-	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
---
+++ restapi/routes/events.go
@@ -0,0 +1,53 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/restapi/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(ctx *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.UserID = 1
+
+	err = event.Save()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create events. Try again later."})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
+}
+
+func getEventById(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event. Try again later."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+	ctx.JSON(http.StatusOK, event)
+}
